cd608cec5296ffd3e679e66acb27df14d16fd501
SanaGithubUber <sanagithububer@gmail.com>
2025-03-23 06:55:07 +0400

    DIArrayProcessingService

---
+++ Services/INumberArrayServices.cs
@@ -0,0 +1,8 @@
+﻿namespace DIArrayProcessingService.Services
+{
+    public interface INumberArrayServices
+    {//interface
+        int[] SortInLoop(int[] numbers);  // Method declaration (no implementation)
+        int[] SortOutsideLoop(int[] numbers);
+    }
+}
